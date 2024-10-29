c6e7f629dd8b74da22c3e65d0e205aebb4af58d5
DanielFelipeFlorezCubides <186082651+DanielFelipeFlorezCubides@users.noreply.github.com>
2024-10-29 12:54:58 -0500

    Solucion ejercicio conversion unidades de longitud

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-# Escriba un programa que calcule el promedio de 4 notas ingresadas por el usuario:
-firstGrade = float(input('Enter the first grade: '))
-secondGrade = float(input('Enter the second grade: '))
-thirdGrade = float(input('Enter the third grade: '))
-fourthGrade = float(input('Enter the fourth grade: '))
-sumAverageGrade = (((firstGrade)+(secondGrade)+(thirdGrade)+(fourthGrade)) / 4)
-print(f"""
-    The next one is the average grade: {sumAverageGrade}
+# Escriba un programa que convierta de centímetros a pulgadas. Una pulgada es igual a 2.54 centímetros.
+centimeter = float(input('Put the centimeters number you want to conver to inches: '))
+INCHES = 2.54
+conversion = centimeter / INCHES
+print(f""" 
+    The conversion is: {round(conversion, 1)} in
 """)
