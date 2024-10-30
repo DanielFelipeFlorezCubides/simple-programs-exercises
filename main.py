b10ec006cca4546d3d715e0d86790b0e7a13da5b
DanielFelipeFlorezCubides <186082651+DanielFelipeFlorezCubides@users.noreply.github.com>
2024-10-30 06:38:41 -0500

    Solucion ejercicio numero invertido

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-# Escriba un programa que convierta de centímetros a pulgadas. Una pulgada es igual a 2.54 centímetros.
-centimeter = float(input('Put the centimeters number you want to conver to inches: '))
-INCHES = 2.54
-conversion = centimeter / INCHES
-print(f""" 
-    The conversion is: {round(conversion, 1)} in
-""")
+# Escriba un programa que pida al usuario un entero de tres dígitos, y entregue el número con los dígitos en orden inverso:
+
+def invertir_numero(numero):
+
+  # Verifica si el número tiene tres dígitos
+  if 100 <= numero <= 999:
+
+    # Obtiene los dígitos individuales
+    centenas = numero // 100
+    decenas = (numero % 100) // 10
+    unidades = numero % 10
+
+    # Invierte los dígitos
+    numero_invertido = unidades * 100 + decenas * 10 + centenas
+
+    return numero_invertido
+  else:
+    return "The number must has 3 digits."
+
+# Obtiene el número del usuario
+numero = int(input("Put a 3 digits number: "))
+
+# Invierte el número y muestra el resultado
+numero_invertido = invertir_numero(numero)
+print("The inverted number is:", numero_invertido)
