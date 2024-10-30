44b0bf10820c4b7b1de0d622a5a35e28ec29acab
DanielFelipeFlorezCubides <186082651+DanielFelipeFlorezCubides@users.noreply.github.com>
2024-10-30 07:04:07 -0500

    Solucion al ejercicio hora futura

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-# Escriba un programa que reciba como entrada las longitudes de los dos catetos a y b
-# de un triángulo rectángulo, y que entregue como salida el largo de la hipotenusa c
-# del triangulo, dado por el teorema de Pitágoras: c2=a2+b2
+# Escriba un programa que pregunte al usuario la hora actual t del reloj y 
+# un número entero de horas h, que indique qué hora marcará el reloj dentro de h horas:
 
-import math
 # Variables declaration input
-a = int(input('Give me the lenght of the first side of the triangle: '))
-b = int(input('Give me the lenght of the second side of the triangle: '))
+t = int(input('Put the current hour: '))
+h = int(input('Put the number of hours you want to go to the future: '))
+result = t + h
 
-# Processing
-c = math.sqrt(((a**2)+(b**2)))
-
-# Output
-print(f'The hypotenuse of the trinagle is: {c}')
+# Processing and output
+if result < 12:
+    print(f'In {h} hours the clock will show: {result}')
+else:
+    result = ((t + h)%12)
+    print(f'In {h} hours the clock will show: {result}')
